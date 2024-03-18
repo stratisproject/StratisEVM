3d221c07fc41950e7d45d0756525362155f47e71
StratisIain <32906542+StratisIain@users.noreply.github.com>
2024-03-18 16:16:24 +0000

    Update Dockerfiles and Docker Compose

---
+++ docker/build.sh
@@ -0,0 +1,4 @@
+docker-compose build --force-rm
+docker-compose -f docker-compose.archive.yml build --force-rm
+docker-compose -f docker-compose.auroria.yml build --force-rm
+docker-compose -f docker-compose.auroria.archive.yml build --force-rm
